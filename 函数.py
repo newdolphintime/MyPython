3e3ad2694970c39c40729e4f9042168730015120
zhangwei123 <1551583950@qq.com>
2017-02-20 23:23:46 +0800

    哇呜哇呜

--- 函数.py
+++ 函数.py
@@ -72,3 +72,6 @@ def person(name, age, **kw):
     print('name:', name, 'age:', age, 'other:', kw)
 person('Michael', 30)
 person('Bob', 35, city='Beijing')
+extra = {'city': 'Beijing', 'job': 'Engineer'}
+print(extra['job'])
+person('Bob', 35, city='Beijing',job=extra['job'])
