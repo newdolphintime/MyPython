4f34d9e3109a46b9bee27722dcb3ebccbbdffaca
zhangwei123 <1551583950@qq.com>
2017-03-14 23:05:03 +0800

    加入文件功能！

--- spider.py
+++ spider.py
@@ -2,8 +2,33 @@
 import requests
 import urllib.request
 from bs4 import BeautifulSoup
+import os
+def mkdir(path):
+    # 引入模块
+    # 去除首位空格
+    path = path.strip()
+    # 去除尾部 \ 符号
+    path = path.rstrip("\\")
+    # 判断路径是否存在
+    # 存在     True
+    # 不存在   False
+    isExists = os.path.exists(path)
+    # 判断结果
+    if not isExists:
+        # 如果不存在则创建目录
+        print(path + ' 创建成功')
+        # 创建目录操作函数
+        os.makedirs(path)
+        return True
+    else:
+        # 如果目录存在则不创建，并提示目录已存在
+        print(path + ' 目录已存在')
+        return False
+#############################################
+#################开始采集数据##################
+#############################################
 baseurl='http://www.bobx.com'
-url = '/av-idol/mikami-yua/'
+url = '/idol/yoshioka-riho/'
 headers = {'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) '
                          'Chrome/51.0.2704.63 Safari/537.36'}
 req = requests.get(url=baseurl+url, headers=headers)
@@ -14,6 +39,11 @@ nameList = bsObj.findAll("a", {"class":"medblack"})
 if nojpUrl is not None:
     print("请切换日链")
     exit()
+path=url.split("/")
+# 定义要创建的目录
+mainpath = "d:\\"+path[2]
+# 调用函数
+mkdir(mainpath)
 for name in nameList:
     album=name.get_text()
     url = name['href']
@@ -21,6 +51,8 @@ for name in nameList:
     print(album)
     print('-------------------------------------------------------------------------------------------')
     print(baseurl+url)
+    branchpath=mainpath+"\\"+album
+    mkdir(branchpath)
     url=baseurl+url
     photoAdress=set()
     while url is not None:
@@ -37,10 +69,21 @@ for name in nameList:
             url=None
     print(len(photoAdress))
     print(photoAdress)
-
+    filepath = branchpath + "\\" + album + ".txt"
+    f = open(filepath,'w')
     for jpgadress in photoAdress:
         url=baseurl+jpgadress
         req = requests.get(url=url, headers=headers)
         bsObj = BeautifulSoup(req.content, "html.parser")
         jpgname = bsObj.find("a", {"class":"smallblack"})
         print(baseurl+jpgname['href'])
+        str=baseurl+jpgname['href']+"\n"
+        f.write(str)
+        f.flush()
+        print("写入成功")
+    f.close()
+
+
+
+
+
