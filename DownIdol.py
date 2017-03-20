5850e46db765378b12085289b895fff491b9a34c
zhangwei123 <1551583950@qq.com>
2017-03-20 23:17:18 +0800

    加入下载功能！

--- DownIdol.py
+++ DownIdol.py
@@ -1,18 +1,19 @@
-import os
+
 import os.path
 import requests
 from contextlib import closing
+
 headers = {'User-Agent' : 'Mozilla/5.0 (X11; Ubuntu; Linux x86_64; rv:46.0) Gecko/20100101 Firefox/46.0',
         'Content-Type': 'application/x-www-form-urlencoded',
         'Connection' : 'Keep-Alive',
         'Accept':'text/html,application/xhtml+xml,application/xml;q=0.9,*/*;q=0.8'}
-rootdir = "D:\\写真\\yoshioka-riho"                               # 指明被遍历的文件夹
+rootdir = "D:\\写真\\yoshioka-riho\\WPB 2015 #27"                               # 指明被遍历的文件夹
 pathSet=set()
 for parent,dirnames,filenames in os.walk(rootdir):    #三个参数：分别返回1.父目录 2.所有文件夹名字（不含路径） 3.所有文件名字
     for filename in filenames:                        #输出文件信息
-        #print ("parent is:" + parent)
-        #print ("filename is:" + filename)
-        #print ("the full name of the file is:" + os.path.join(parent,filename)) #输出文件路径信息
+        # print ("parent is:" + parent)
+        # print ("filename is:" + filename)
+        # print ("the full name of the file is:" + os.path.join(parent,filename)) #输出文件路径信息
         pathSet.add(os.path.join(parent,filename))
 #print(pathSet)
 for path in pathSet:
@@ -22,10 +23,11 @@ for path in pathSet:
     f = open(path, 'r')
     for line in f.readlines():
         print(line.strip())
+        url=line.strip()
         print(basePath+'\\'+line.split('/')[-1])
         filePath=basePath+'\\'+line.split('/')[-1]
         filePath=filePath.strip()
-        with closing(requests.get(line.strip(), stream=True,headers=headers)) as response:
+        with closing(requests.get(url=url, stream=True,headers=headers)) as response:
             chunk_size = 1024  # 单次请求最大值
             content_size = int(response.headers['content-length']) # 内容体总大小
             print("大小"+str(content_size))
